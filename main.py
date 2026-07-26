fa4b73fddaf9e45f2476ad82be4d18cabbb7629c
Radii Yevstratov <r.yevstratov.projects@gmail.com>
2026-07-26 14:30:13 +0200

    Update error handling (Missing keys, Not a dict, ValueError-s)

--- main.py
+++ main.py
@@ -2,29 +2,66 @@ import os
 import sys
 import argparse
 import json
+from datetime import datetime, timedelta
+
 
 
 def main():
-    path, time, json_format = get_args()
-    get_data(path=path)
+    path, window, json_format = get_args()
+    data = get_data(path=path)
+    validate(data=data)
+
 
 def get_args():
     parser=argparse.ArgumentParser()
     parser.add_argument("input", help="Enter a path where are the logs are", type=str)
-    parser.add_argument("--time", help="Enter a time of interval you would like to check. (in minutes)", default=10, type=int)
+    parser.add_argument("--window", help="Enter a window of interval you would like to check. (in minutes)", default=10, type=int)
     parser.add_argument("--json", help="Return result in json format", action="store_true")
     args = parser.parse_args()
-    return args.input, args.time, args.json
+    return args.input, args.window, args.json
 
 def get_data(path):
+
+    if not os.path.isdir(path):
+        print(f"Error: '{path}' is not a directory or does not exist.", file=sys.stderr)
+        sys.exit(1)
+
     try:
         with open (path, "r") as file:
             data = json.load(file)
-    except (FileNotFoundError, FileExistsError) as e:
-        print(e)
-        sys.exit(2)
+        return data
     except json.decoder.JSONDecodeError as e:
         print(e)
         sys.exit(2)
+    except PermissionError as e:
+        print(e)
+        sys.exit(2)
+
+
+def validate(data):
+    EVENT_TYPES = ["successful_login", "failed_login"]
+    CHECK_LOGS_BY_IP = []
+    for l in data:
+
+        if type(l) is not dict:
+            print("not dict")
+            sys.exit(2)
+
+        elif len(l) != 4:
+            print("Missing key")
+            sys.exit(2)
+
+        else:
+            for key, value in l.items():
+                if key == "timestamp":
+                    try:
+                        value = datetime.fromisoformat(value)
+                        l["timestamp"] = value
+                    except ValueError as e:
+                        print(e)
+                        sys.exit(2)
+
+
+
 if __name__== "__main__":
     main()
