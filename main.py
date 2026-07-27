203570f05d08c8c77af55b86891481c678c550d8
Radii Yevstratov <r.yevstratov.projects@gmail.com>
2026-07-27 12:25:47 +0200

    Edit storing of data for every IP

--- main.py
+++ main.py
@@ -65,54 +65,65 @@ def validate(data):
                     except ValueError as e:
                         print(e)
                         sys.exit(2)
-            data = check_ip(l["ip"], l["timestamp"], l["event"], CHECK_LOGS_BY_IP)
+            
+            data = check_ip(l["ip"], l["timestamp"], l["event"], l["user"], CHECK_LOGS_BY_IP)
 
     return data
 
 
-def check_ip(ip, time, event, logs):
+def check_ip(ip, time, event, username, logs):
     for d in logs:
-        if  ip in d:
-            d[ip].append([time, event])
+        if  ip in d["ip"]:
+            d["event"].append([time, event])
+            if username not in d["user"]:
+                d["user"].append(username)
+
             for d in logs:
                 for ip, events in d.items():
-                    events.sort(key=lambda x: x[0])
+                    d["event"].sort(key=lambda event: event[0])
             return logs
-    logs.append({ip: [[time, event]]})
+    logs.append({"ip": ip, "event": [[time, event]], "user": [username]})
+
     return logs
 
 def analyze(data, time_window):
+
+    type_b = []
+    type_a = []
     for d in data:
         failed_count = 0
         error_type = None
-        for key, values in d.items():
+        ip = d["ip"]
+        events = d["event"]
+        users = d["user"]
+        print(ip)
+        print(events)
+        print(users)
+        # for key, values in d.items():    
+        #         for i, value in enumerate(values):
+        #             time = value[0]
+        #             event =  value[1]
 
-            for i, value in enumerate(values):
+        #             j = i
+        #             max_time = None
+        #             finishing_time = time + timedelta(minutes=time_window)
+        #             while j < len(values) and values[j][0] < finishing_time:
 
-                time = value[0]
-                event =  value[1]
+        #                 if max_time is None or values[j][0] > max_time:
+        #                     max_time = values[j][0]
+        #                 j+= 1
 
-                j = i
-                max_time = None
-                failed_attempts = 0
-                finishing_time = time + timedelta(minutes=time_window)
-                while j < len(values) and values[j][0] < finishing_time:
+        #                 if event == "failed_login":
+        #                     failed_count += 1
+        #                 if failed_count >= 5 and event == "successful_login":
+        #                     error_type = "Type B"
+        #                     type_b.append({"ip": key, "failed": failed_count, "users": d["user"]})
+        #                     failed_count = 0
+        #                 elif failed_count >= 5 and event != "successful_login":
+        #                     error_type = "Type A"
+        #                 elif failed_count < 5 and event == "successful_login":
+        #                     failed_count = 0
 
-                    if max_time is None or values[j][0] > max_time:
-                        max_time = values[j][0]
-                    j+= 1
-
-                    if event == "failed_login":
-                        failed_count += 1
-                    if failed_count >= 5 and event == "successful_login":
-                        error_type = "Type B"
-                        failed_count = 0
-                    elif failed_count >= 5 and event != "successful_login":
-                        error_type = "Type A"
-                    elif failed_count < 5 and event == "successful_login":
-                        failed_count = 0
-
-        print(error_type)
 
 
 
